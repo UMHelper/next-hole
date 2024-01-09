c30144943c676da387cb01cc228d80dc35608221
Box <me@boxz.dev>
2024-01-09 14:19:44 +0800

    clean

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,9 +2,9 @@ import { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'What2REG @UM 澳大選咩課',
-    short_name: 'What2REG @UM',
-    description: 'Course review platform for University of Macau',
+    name: 'WHOLE @UM',
+    short_name: 'WHOLE @UM',
+    description: 'BBS platform for University of Macau',
     start_url: '/',
     display: 'standalone',
     background_color: '#fff',
--- lib/database/get-statistics.ts
+++
@@ -1,9 +0,0 @@
-import supabase from '@/lib/database/database';
-
-export const getStatistics = async () => {
-
-    const { data, error } = await supabase.from('statistics')
-                            .select('*')
-        
-    return data;
-}
